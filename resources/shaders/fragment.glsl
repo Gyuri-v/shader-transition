93b3d314529a21e8ca1731eaae656d7deae28905
Gyuri-v <fnffkdll@naver.com>
2023-11-06 17:57:36 +0900

    ate the source and add the comment

--- resources/shaders/fragment.glsl
+++ resources/shaders/fragment.glsl
@@ -15,7 +15,10 @@ void main () {
 
   float threshold = 0.2;
   float progress = u_progress * (1.0 + threshold * 2.0) - threshold; // -0.2 ~ 1.2
-  float transition = clamp((progress - transitionColor.r) * (1.0 / threshold), 0.0, 1.0);
+  float transition = clamp((progress - transitionColor.r) * (1.0 / threshold), 0.0, 1.0); 
+  // clamp(a:현재값, b:최소값, c:최대값) 
+  // clamp 하지않으면 transitionColor.r=0 -> -1~6 / itionColor.r=1 -> -6~1 --- 각각 다르게 증가할듯
+  // threshold로 위의 수식을 해주지 않으면 transitionColor.r=0 -> 0~1 - 얘만 증가하고 / itionColor.r=1 -> -1~0 -> 0->0
 
   gl_FragColor = mix(color1, color2, transition);
 }
